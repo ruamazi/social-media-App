ee1ca71ae49c10f5b3a662f20e1e904b2f0660f3
mohssin <barcalover016@gmail.com>
2023-10-21 16:14:59 +0100

    trying to fix config file

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,6 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://social-media-app-vercel-gamma.vercel.app",
-        changeOrigin: true,
-        secure: false,
       },
     },
   },
